d0d9a75fbd3bb091b56f84ac54bcc968cee887a1
MersalHussin <mersalhussin@gmail.com>
2024-07-15 15:18:43 +0300

    uplaoad files

---
+++ main.js
@@ -0,0 +1,7 @@
+let burgerMenu = document.querySelector(".burger-menu");
+let mobileMenu = document.querySelector(".on-mobile");
+
+function toggleMenu(){
+    burgerMenu.classList.toggle("opened")
+    mobileMenu.classList.toggle("opened")
+}
